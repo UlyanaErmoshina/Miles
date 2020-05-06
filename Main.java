175e2cf51b42e94b5d4f86c6fbe4967f4cbce577
Ulyana Ermoshina <uermoshina@inbox.ru>
2020-05-06 15:45:16 +0300

    In commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketprice = 1500;
+        int priceonemile = 20;
+
+        int mile = ticketprice / priceonemile;
+
+        System.out.println(mile);
+    }
+}
